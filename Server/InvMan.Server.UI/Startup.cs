d2707226a539370c5489c7a42d54fd655319f6dc
CIRCULARKA <donkardon@list.ru>
2021-09-13 00:09:22 +0500

    Remove unused namespaces

--- Server/InvMan.Server.UI/Startup.cs
+++ Server/InvMan.Server.UI/Startup.cs
@@ -4,9 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 using InvMan.Server.Database;
-using InvMan.Server.Domain;
 
 namespace InvMan.Server.UI
 {

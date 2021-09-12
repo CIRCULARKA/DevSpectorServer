179f49255bda3b743c074608994ab63bf7f51da4
CIRCULARKA <donkardon@list.ru>
2021-09-12 15:36:55 +0500

    Update model configs

--- Server/InvMan.Server.Database/Configurations/DeviceTypeConfiguration.cs
+++ Server/InvMan.Server.Database/Configurations/DeviceTypeConfiguration.cs
@@ -9,15 +9,8 @@ namespace InvMan.Server.Database.Configurations
 		public void Configure(EntityTypeBuilder<DeviceType> builder)
 		{
 			builder.HasKey(dt => dt.ID);
+			builder.Property(dt => dt.ID).UseIdentityColumn();
 			builder.Property(dt => dt.Name).IsRequired();
-			builder.HasData(
-				new DeviceType[]
-				{
-					new DeviceType { ID = 1, Name = "Персональный компьютер" },
-					new DeviceType { ID = 2, Name = "Сервер" },
-					new DeviceType { ID = 3, Name = "Коммутатор" }
-				}
-			);
 		}
 	}
 }
--- Server/InvMan.Server.Database/Configurations/IPAddressConfiguration.cs
+++ Server/InvMan.Server.Database/Configurations/IPAddressConfiguration.cs
@@ -10,40 +10,7 @@ namespace InvMan.Server.Database.Configurations
 		{
 			builder.HasKey(ia => ia.ID);
 			builder.Property(ia => ia.ID).UseIdentityColumn();
-			builder.Property(ia => ia.DeviceID).IsRequired(false);
 			builder.HasIndex(ia => ia.Address).IsUnique();
-			builder.Ignore(ia => ia.IsAvailable);
-
-			builder.HasData(
-				new IPAddress
-				{
-					ID = 1,
-					Address = "198.22.33.1",
-					DeviceID = 1
-				},
-				new IPAddress
-				{
-					ID = 2,
-					Address = "198.22.33.2",
-					DeviceID = 1
-				},
-				new IPAddress
-				{
-					ID = 3,
-					Address = "198.22.33.3",
-					DeviceID = 3
-				},
-				new IPAddress
-				{
-					ID = 4,
-					Address = "198.22.33.4"
-				},
-				new IPAddress
-				{
-					ID = 5,
-					Address = "198.22.33.5"
-				}
-			);
 		}
 	}
 }

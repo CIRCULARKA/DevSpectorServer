3188261a9f01022c620ed6b29d35ea6b557258ab
CIRCULARKA <donkardon@list.ru>
2021-09-25 01:58:38 +0500

    Update assembly info

--- Mobile/InvMan.Mobile.Android/Properties/AssemblyInfo.cs
+++ Mobile/InvMan.Mobile.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InvMan.InvMan.Mobile.Android")]
+[assembly: AssemblyTitle("InvMan.Mobile.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InvMan.InvMan.Mobile.Android")]
+[assembly: AssemblyProduct("InvMan.Mobile.Android")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

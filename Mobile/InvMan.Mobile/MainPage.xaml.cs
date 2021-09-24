45bb3ed4fd05f9490620ee19ad31173e89f6cec6
CIRCULARKA <donkardon@list.ru>
2021-09-25 02:23:36 +0500

    Refactor code

--- Mobile/InvMan.Mobile/MainPage.xaml.cs
+++ Mobile/InvMan.Mobile/MainPage.xaml.cs
@@ -1,52 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using Xamarin.Forms;
-using System.Net.Http.Headers;
-using InvMan.Mobile.Infrastructure;
-
+using InvMan.Common.Models;
 
 namespace InvMan.Mobile
 {
     public partial class MainPage : ContentPage
     {
-        public List<Аppliance> Аppliances { get; set; }
+        public List<Appliance> Devices { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
 
-            Аppliances = new List<Аppliance>
+            Devices = new List<Appliance>
             {
-                new Аppliance { Name  = "COMMUTATOR-1", Device_type = "Коммутатор", Inventory_number = "NSGK123123"},
-                new Аppliance { Name  = "MAIN-SERVER", Device_type = "Сервер", Inventory_number = "NSGK052674"},
-                new Аppliance { Name  = "IVAN-PC", Device_type = "Персональный компьютер", Inventory_number = "NSGK998784"},
+                new Appliance(1, "COMMUTATOR-1", "Коммутатор", "NSGK123123", null),
+                new Appliance(2, "MAIN-SERVER", "Сервер", "NSGK032543", null),
+                new Appliance(3, "COMMUTATOR-1", "Коммутатор", "NSGK3526901", null),
             };
             this.BindingContext = this;
-
-            /*LoadData();*/
         }
 
         public async void OnItemTapped(object sende, ItemTappedEventArgs e)
         {
-            Аppliance selectedАppliance = e.Item as Аppliance;
+            Appliance selectedАppliance = e.Item as Appliance;
             if (selectedАppliance != null)
-                await DisplayAlert("Выбранное устройство", $"{ selectedАppliance.Device_type } - { selectedАppliance.Inventory_number }", "OK");
+                await DisplayAlert("Выбранное устройство", $"{selectedАppliance.Type} - {selectedАppliance.InventoryNumber}", "OK");
         }
-
-
-        /*public async void LoadData()
-        {
-            var client = new HttpClient();
-
-            Task<string> task = (await client.GetAsync("http://10.0.2.2:5000/api/devices")).Content.ReadAsStringAsync();
-
-            result.Text = await task;
-        }*/
     }
 
 }

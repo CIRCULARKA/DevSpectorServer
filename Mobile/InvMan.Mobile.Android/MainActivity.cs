62ce4a2bb8def1922f40c7de28ab7d999f20645b
CIRCULARKA <donkardon@list.ru>
2021-09-25 02:11:37 +0500

    Refactore MainActivity

--- Mobile/InvMan.Mobile.Android/MainActivity.cs
+++ Mobile/InvMan.Mobile.Android/MainActivity.cs
@@ -5,9 +5,11 @@ using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
 
-namespace InvMan.Mobile.Droid
+namespace InvMan.Mobile.Android
 {
-    [Activity(Label = "InvMan.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "InvMan.Mobile", Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -19,6 +21,7 @@ namespace InvMan.Mobile.Droid
             LoadApplication(new App());
 
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);

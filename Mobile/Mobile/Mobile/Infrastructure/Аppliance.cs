24d6e5303815675d7455b146a414d3860cce6bc1
4view <akochergin.2002@gmail.com>
2021-09-24 21:44:12 +0500

    Init mobile

---
+++ Mobile/Mobile/Mobile/Infrastructure/Аppliance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobile.Infrastructure
+{
+    public class Аppliance
+    {
+        public string Name { get; set; }
+
+        public string Device_type { get; set; }
+
+        public string Inventory_number { get; set; }
+    }
+}

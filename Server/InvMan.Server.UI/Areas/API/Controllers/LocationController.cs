d434f9a51f6d4955d1c5d6d02716025bb0e07c70
CIRCULARKA <donkardon@list.ru>
2021-09-25 15:07:55 +0500

    Implement LocationController

---
+++ Server/InvMan.Server.UI/Areas/API/Controllers/LocationController.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using InvMan.Server.Domain;
+
+namespace InvMan.Server.UI.API.Controllers
+{
+	public class LocationController : ApiController
+	{
+		private readonly ILocationRepository _repository;
+
+		public LocationController(ILocationRepository repo) =>
+			_repository = repo;
+
+		[HttpGet("{housingID}")]
+		public IEnumerable<string> Get(int housingID) =>
+			_repository.GetHousingCabinets(housingID).Select(c => c.Name);
+
+		[HttpGet]
+		public IEnumerable<string> Get() =>
+			_repository.Housings.Select(h => h.Name);
+	}
+}

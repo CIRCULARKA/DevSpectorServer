eb3efae17a21fc632b62bdbecc51102b7b30d38d
CIRCULARKA <donkardon@list.ru>
2021-09-28 20:57:38 +0500

    Update mobile project

--- Mobile/InvMan.Mobile/App.xaml.cs
+++ Mobile/InvMan.Mobile/App.xaml.cs
@@ -4,26 +4,25 @@ using Xamarin.Forms.Xaml;
 
 namespace InvMan.Mobile
 {
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
 
-            MainPage = new MainPage();
+			MainPage = new MainPage();
+		}
 
-        }
+		protected override void OnStart()
+		{
+		}
 
-        protected override void OnStart()
-        {
-        }
+		protected override void OnSleep()
+		{
+		}
 
-        protected override void OnSleep()
-        {
-        }
-
-        protected override void OnResume()
-        {
-        }
-    }
+		protected override void OnResume()
+		{
+		}
+	}
 }
--- Mobile/InvMan.Mobile/MainPage.xaml.cs
+++ Mobile/InvMan.Mobile/MainPage.xaml.cs
@@ -1,55 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
-using InvMan.Common.SDK.Models;
 
 namespace InvMan.Mobile
 {
-    public partial class MainPage : ContentPage
-    {
-        public List<Appliance> Devices { get; set; }
-
-        public MainPage()
-        {
-            InitializeComponent();
-
-            Devices = new List<Appliance>
-            {
-                new Appliance(
-                    id: 1,
-                    networkName: "COMMUTATOR-1",
-                    type: "Коммутатор",
-                    inventoryNumber: "NSKG123123",
-                    housing: "hous1",
-                    cabinet: "cab1",
-                    ipAddresses: null
-                ),
-                new Appliance(
-                    id: 1,
-                    networkName: "IVAN-PC",
-                    type: "ПК",
-                    inventoryNumber: "NSGK60231",
-                    housing: "hous2",
-                    cabinet: "cab2",
-                    ipAddresses: null
-                ),
-                new Appliance(
-                    id: 1,
-                    networkName: "MAIN-SERVER",
-                    type: "Сервер",
-                    inventoryNumber: "NSGK53412e",
-                    housing: "hous3",
-                    cabinet: "cab3",
-                    ipAddresses: null
-                ),
-            };
-            this.BindingContext = this;
-        }
-
-        public async void OnItemTapped(object sende, ItemTappedEventArgs e)
-        {
-            Appliance selectedАppliance = e.Item as Appliance;
-            if (selectedАppliance != null)
-                await DisplayAlert("Выбранное устройство", $"{selectedАppliance.Type} - {selectedАppliance.InventoryNumber}", "OK");
-        }
-    }
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
 }

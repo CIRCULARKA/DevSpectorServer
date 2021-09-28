52f3ef75cd33fcafed574ee56317dcab00c5b6c2
CIRCULARKA <donkardon@list.ru>
2021-09-29 00:14:47 +0500

    Add download circle

---
+++ Desktop/InvMan.Desktop.Common/DownloadCircle/DownloadCircle.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace InvMan.Desktop.Common
+{
+	public partial class DownloadCircle : UserControl
+	{
+		public DownloadCircle()
+		{
+			InitializeComponent();
+		}
+	}
+}

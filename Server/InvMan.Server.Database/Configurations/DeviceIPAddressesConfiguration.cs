7a4552d08663672981bfae5fa15a4b0c937c17fa
CIRCULARKA <donkardon@list.ru>
2021-09-28 13:45:04 +0500

    Add DeviceIPAddresses model configuration

---
+++ Server/InvMan.Server.Database/Configurations/DeviceIPAddressesConfiguration.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using InvMan.Server.Domain.Models;
+
+namespace InvMan.Server.Database.Configurations
+{
+	public class DeviceIPAddressesConfiguration : IEntityTypeConfiguration<DeviceIPAddresses>
+	{
+		public void Configure(EntityTypeBuilder<DeviceIPAddresses> builder)
+		{
+			builder.HasKey(dt => dt.ID);
+			builder.Property(dt => dt.ID).UseIdentityColumn();
+			builder.Property(dt => dt.DeviceID).IsRequired();
+			builder.Property(dt => dt.IPAddressID).IsRequired();
+			var firstDeviceIPs = new List<DeviceIPAddresses>();
+			int i = 1;
+			for (; i <= 3; i++)
+				firstDeviceIPs.Add(
+					new DeviceIPAddresses { ID = 1, DeviceID = 1, IPAddressID = i }
+				);
+
+			var secondDeviceIPs = new List<DeviceIPAddresses>();
+			for (; i <= 5; i++)
+				secondDeviceIPs.Add(
+					new DeviceIPAddresses { ID = 1, DeviceID = 1, IPAddressID = i }
+				);
+
+			var thirdDeviceIPs = new List<DeviceIPAddresses>();
+			for (; i <= 9; i++)
+				thirdDeviceIPs.Add(
+					new DeviceIPAddresses { ID = 1, DeviceID = 1, IPAddressID = i }
+				);
+
+			var result = new List<DeviceIPAddresses>();
+			result.AddRange(firstDeviceIPs);
+			result.AddRange(secondDeviceIPs);
+			result.AddRange(thirdDeviceIPs);
+			builder.HasData(result);
+		}
+	}
+}

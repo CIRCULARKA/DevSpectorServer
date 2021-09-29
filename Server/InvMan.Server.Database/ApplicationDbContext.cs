80b009f443d783fcd04d37a761514ef952450c4e
CIRCULARKA <donkardon@list.ru>
2021-09-30 01:26:13 +0500

    Remove db creating in context constructor

--- Server/InvMan.Server.Database/ApplicationDbContext.cs
+++ Server/InvMan.Server.Database/ApplicationDbContext.cs
@@ -1,6 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using InvMan.Server.Domain.Models;
-using InvMan.Server.Database.Configurations;
 
 namespace InvMan.Server.Database
 {
@@ -11,9 +9,6 @@ namespace InvMan.Server.Database
 		public ApplicationDbContext(string connectionString)
 		{
 			_conntectionString = connectionString;
-
-			Database.EnsureDeleted();
-			Database.EnsureCreated();
 		}
 
 		protected override void OnConfiguring(DbContextOptionsBuilder builder) =>

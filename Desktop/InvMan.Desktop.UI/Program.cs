e7de75268a37f9199ea5cee1054642cf1eeea9ef
CIRCULARKA <donkardon@list.ru>
2021-12-07 20:45:42 +0500

    Change avalonia project structure

---
+++ Desktop/InvMan.Desktop.UI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.ReactiveUI;
+
+namespace InvMan.Desktop.UI
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace()
+                .UseReactiveUI();
+    }
+}

627a1e42c5c24501dc0390053662a55b3cb3354c
CIRCULARKA <donkardon@list.ru>
2021-12-11 01:23:22 +0500

    Remove ViewLocator and configure IOC kernel in App (prev. commit)

--- Desktop/InvMan.Desktop.UI/App.axaml.cs
+++ Desktop/InvMan.Desktop.UI/App.axaml.cs
@@ -4,30 +4,34 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using InvMan.Desktop.UI.ViewModels;
 using InvMan.Desktop.UI.Views;
-using InvMan.Common.SDK;
+using InvMan.Desktop.Service.DependencyInjection;
 
 namespace InvMan.Desktop.UI
 {
     public class App : Application
     {
+        private readonly IKernel _kernel;
+
+        public App()
+        {
+            _kernel = new StandardKernel(
+                new ViewModelsModule(),
+                new SdkModule(),
+                new RootModule()
+            );
+        }
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
         }
 
-        public IKernel Kernel { get; init; }
-
         public override void OnFrameworkInitializationCompleted()
         {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = Kernel.Get<MainWindowViewModel>()
-                };
-            }
-
             base.OnFrameworkInitializationCompleted();
+
+            var desktop = ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
+            desktop.MainWindow = _kernel.Get<MainView>();
         }
     }
 }

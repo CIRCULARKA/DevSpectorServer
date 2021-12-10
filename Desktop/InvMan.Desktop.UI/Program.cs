f956a98f88923c243dc73a032ceb2718b6998d46
CIRCULARKA <donkardon@list.ru>
2021-12-11 01:20:26 +0500

    Remove IOC initialization from application root

--- Desktop/InvMan.Desktop.UI/Program.cs
+++ Desktop/InvMan.Desktop.UI/Program.cs
@@ -1,32 +1,20 @@
 ﻿using System;
 using Avalonia;
 using Avalonia.ReactiveUI;
-using Ninject;
-using InvMan.Desktop.Service.DependencyInjection;
 
 namespace InvMan.Desktop.UI
 {
 	class Program
 	{
-		private static IKernel _kernel;
-
 		[STAThread]
 		public static void Main(string[] args)
 		{
-			_kernel = new StandardKernel(
-                new RootModule(),
-                new ViewModelsModule(),
-                new SdkModule()
-            );
-
 			BuildAvaloniaApp().
 				StartWithClassicDesktopLifetime(args);
 		}
 
 		public static AppBuilder BuildAvaloniaApp() =>
-			AppBuilder.Configure<App>(
-				() => new App() { Kernel = _kernel }
-			).
+			AppBuilder.Configure<App>().
 				UsePlatformDetect().
 				LogToTrace().
 				UseReactiveUI();

3a8f40113520d323c85f34530a5aad8845ceb574
CIRCULARKA <donkardon@list.ru>
2021-12-11 01:25:14 +0500

    Update DevicesListView

---
+++ Desktop/InvMan.Desktop.UI/Views/DevicesList.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using InvMan.Desktop.UI.ViewModels;
+
+namespace InvMan.Desktop.UI.Views
+{
+    public partial class DevicesList : UserControl
+    {
+        public DevicesList() { }
+
+        public DevicesList(IDevicesListViewModel viewModel)
+        {
+            InitializeComponent();
+
+            DataContext = viewModel;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}

95745badf15ccb6d6833322e3b046382fdc53825
CIRCULARKA <donkardon@list.ru>
2021-12-12 15:35:52 +0500

    Create validator for devices

---
+++ Server/InvMan.Server.UI/Validators/DeviceValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+using InvMan.Server.Domain.Models;
+
+namespace InvMan.Server.UI.Validators
+{
+    public class DeviceValidator : AbstractValidator<Device>
+    {
+        public DeviceValidator()
+        {
+            RuleFor(d => d.ID).NotEmpty();
+            RuleFor(d => d.TypeID).NotEmpty();
+            RuleFor(d => d.LocationID).NotEmpty();
+
+            RuleFor(d => d.InventoryNumber).
+                NotEmpty().Length(10, 20);
+            RuleFor(d => d.NetworkName).NotEmpty().Length(3, 40);
+        }
+    }
+}

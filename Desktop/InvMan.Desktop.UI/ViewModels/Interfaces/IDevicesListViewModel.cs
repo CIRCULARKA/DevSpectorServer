9209068e24416b588e2c0b7ab7a043cae3031350
CIRCULARKA <donkardon@list.ru>
2022-01-16 15:50:14 +0500

    Update view model interfaces

--- Desktop/InvMan.Desktop.UI/ViewModels/Interfaces/IDevicesListViewModel.cs
+++ Desktop/InvMan.Desktop.UI/ViewModels/Interfaces/IDevicesListViewModel.cs
@@ -1,4 +1,16 @@
+using System.Collections.ObjectModel;
+using InvMan.Common.SDK.Models;
+
 namespace InvMan.Desktop.UI.ViewModels
 {
-    public interface IDevicesListViewModel { }
+    public interface IDevicesListViewModel
+    {
+        ObservableCollection<Appliance> Appliances { get; set; }
+
+        bool AreAppliancesLoaded { get; set; }
+
+        bool AreThereAppliances { get; set; }
+
+        string NoAppliancesMessage { get; set; }
+    }
 }

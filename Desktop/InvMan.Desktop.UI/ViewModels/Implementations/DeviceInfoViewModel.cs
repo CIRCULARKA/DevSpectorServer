5bd3c88bb8fdee2b2e338ad92a9a9aec5c5adf7c
CIRCULARKA <donkardon@list.ru>
2022-01-17 23:03:55 +0500

    Populate DeviceInfo VM

--- Desktop/InvMan.Desktop.UI/ViewModels/Implementations/DeviceInfoViewModel.cs
+++ Desktop/InvMan.Desktop.UI/ViewModels/Implementations/DeviceInfoViewModel.cs
@@ -7,17 +7,56 @@ namespace InvMan.Desktop.UI.ViewModels
     {
         private string _networkName;
 
+        private string _inventoryNumber;
+
+        private string _type;
+
+        private string _housing;
+
+        private string _cabinet;
+
         public DeviceInfoViewModel() { }
 
         public string NetworkName
         {
-            get { return _networkName == null ? "Устройство не выбрано" : _networkName; }
+            get { return _networkName == null ? "N/A" : _networkName; }
             set => this.RaiseAndSetIfChanged(ref _networkName, value);
+        }
+
+        public string InventoryNumber
+        {
+            get { return _inventoryNumber == null ? "N/A" : _inventoryNumber; }
+            set => this.RaiseAndSetIfChanged(ref _inventoryNumber, value);
+        }
+
+        public string Type
+        {
+            get { return _type == null ? "N/A" : _type; }
+            set => this.RaiseAndSetIfChanged(ref _type, value);
+        }
+
+        public string Housing
+        {
+            get { return _housing == null ? "N/A" : _housing; }
+            set => this.RaiseAndSetIfChanged(ref _housing, value);
+        }
+
+        public string Cabinet
+        {
+            get { return _cabinet == null ? "N/A" : _cabinet; }
+            set => this.RaiseAndSetIfChanged(ref _cabinet, value);
         }
 
         public void UpdateDeviceInformation(Appliance target)
         {
             NetworkName = target.NetworkName;
+
+            InventoryNumber = target.InventoryNumber;
+
+            Type = target.Type;
+
+            Housing = target.Housing;
+            Cabinet = target.Cabinet;
         }
     }
 }

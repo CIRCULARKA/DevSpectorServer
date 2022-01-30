dcba1127c0cb4f66651d6769e9309b4d1ecef67b
CIRCULARKA <donkardon@list.ru>
2022-01-30 18:35:25 +0500

    Add "Content" property

--- Desktop/InvMan.Desktop.Common/ModernMenu/ModernMenu.axaml.cs
+++ Desktop/InvMan.Desktop.Common/ModernMenu/ModernMenu.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 
 namespace InvMan.Desktop.UI.Views.Shared
@@ -6,7 +7,10 @@ namespace InvMan.Desktop.UI.Views.Shared
     public class ModernMenu : TemplatedControl
     {
         public static readonly AvaloniaProperty<string> TitleProperty =
-            AvaloniaProperty.Register<ModernMenu, string>("Title", "Menu Title");
+            AvaloniaProperty.Register<ModernMenu, string>(nameof(Title), "Title");
+
+        public static readonly AvaloniaProperty<object> ContentProperty =
+            AvaloniaProperty.Register<ModernMenu, object>(nameof(Content));
 
         public ModernMenu() { }
 
@@ -14,6 +18,12 @@ namespace InvMan.Desktop.UI.Views.Shared
         {
             get => GetValue(TitleProperty) as string;
             set => SetValue(TitleProperty, value);
+        }
+
+        public object Content
+        {
+            get => GetValue(ContentProperty);
+            set => SetValue(ContentProperty, value);
         }
     }
 }

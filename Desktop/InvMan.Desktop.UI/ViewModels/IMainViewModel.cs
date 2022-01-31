5effb103cd34a35cfaa4107ece2069e1f0ca92b9
CIRCULARKA <donkardon@list.ru>
2022-01-31 22:28:10 +0500

    Remove unused property from IMAinViewModel

--- Desktop/InvMan.Desktop.UI/ViewModels/IMainViewModel.cs
+++ Desktop/InvMan.Desktop.UI/ViewModels/IMainViewModel.cs
@@ -1,9 +1,5 @@
-using Avalonia.Controls;
 
 namespace InvMan.Desktop.UI.ViewModels
 {
-    public interface IMainViewModel
-    {
-        UserControl MainMenu { get; }
-    }
+    public interface IMainViewModel { }
 }

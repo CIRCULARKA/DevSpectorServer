506da875df55fc57de80272fce0a7aec0b8f4b39
CIRCULARKA <donkardon@list.ru>
2022-02-01 09:44:48 +0500

    Add some more properties

--- Desktop/InvMan.Desktop.Common/ModernMenu/ModernMenu.axaml.cs
+++ Desktop/InvMan.Desktop.Common/ModernMenu/ModernMenu.axaml.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using Avalonia;
+using Avalonia.Input;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 
@@ -7,6 +11,8 @@ namespace InvMan.Desktop.UI.Views.Shared
 {
     public class ModernMenu : TemplatedControl
     {
+        private object _currentContent;
+
         private Button _mainButton;
 
         private List<ModernMenuItem> _menuItems;
@@ -17,9 +23,25 @@ namespace InvMan.Desktop.UI.Views.Shared
         public static readonly DirectProperty<ModernMenu, List<ModernMenuItem>> MenuItemsProperty =
             AvaloniaProperty.RegisterDirect<ModernMenu, List<ModernMenuItem>>(nameof(MenuItems), o => o.MenuItems);
 
+        public static readonly DirectProperty<ModernMenu, object> CurrentContentProperty =
+            AvaloniaProperty.RegisterDirect<ModernMenu, object>(nameof(MenuItems), o => o.CurrentContent);
+
+        public static readonly StyledProperty<int> SelectedIndexProperty =
+            AvaloniaProperty.Register<ModernMenu, int>(nameof(SelectedIndex), 0);
+
         public ModernMenu()
         {
             _menuItems = new List<ModernMenuItem>();
+
+            SelectedIndexProperty.Changed.Subscribe(ChangeCurrentContent);
+        }
+
+        private void ChangeCurrentContent(AvaloniaPropertyChangedEventArgs<int> info)
+        {
+            var incomingValue = info.NewValue.Value;
+            if (incomingValue >= _menuItems.Count) return;
+
+            CurrentContent = _menuItems[info.NewValue.Value];
         }
 
         public string Title
@@ -28,18 +50,42 @@ namespace InvMan.Desktop.UI.Views.Shared
             set => SetValue(TitleProperty, value);
         }
 
+        public int SelectedIndex
+        {
+            get => (int)GetValue(SelectedIndexProperty);
+            set => SetValue(SelectedIndexProperty, value);
+        }
+
+        public object CurrentContent
+        {
+            get => _currentContent;
+            set => SetAndRaise<object>(CurrentContentProperty, ref _currentContent, value);
+        }
+
         public List<ModernMenuItem> MenuItems => _menuItems;
 
-        public void Add(ModernMenuItem item)
-        {
+        public void Add(ModernMenuItem item) =>
             MenuItems.Add(item);
-        }
 
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
             base.OnApplyTemplate(e);
 
             _mainButton = GetTemplateControl<Button>(e, "PART_mainButton");
+
+        }
+
+        protected async override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            // I don't know why but I found very weird solution about why CurrentContent gets default value of SelectedIndexProperty
+            // instead of desired value that was set in the MainView.axaml
+            // and I don't know how does it work
+            // HELP MEEE
+            await Task.Run(() => Thread.Sleep(1));
+
+            CurrentContent = _menuItems[GetValue(SelectedIndexProperty)].Content;
         }
 
         private T GetTemplateControl<T>(TemplateAppliedEventArgs e, string controlName)

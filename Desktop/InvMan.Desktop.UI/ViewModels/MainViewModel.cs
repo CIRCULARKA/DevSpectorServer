c40e82e67c6ca9ae440b7f375193a5d98a6e2be5
CIRCULARKA <donkardon@list.ru>
2022-02-01 10:53:04 +0500

    Remove unused properties

--- Desktop/InvMan.Desktop.UI/ViewModels/MainViewModel.cs
+++ Desktop/InvMan.Desktop.UI/ViewModels/MainViewModel.cs
@@ -1,15 +1,10 @@
-using System.Reactive;
-using System.Collections.Generic;
 using Avalonia.Controls;
-using ReactiveUI;
 using InvMan.Desktop.UI.Views;
 
 namespace InvMan.Desktop.UI.ViewModels
 {
     public class MainViewModel : ViewModelBase, IMainViewModel
     {
-        private UserControl _currentContent;
-
         public MainViewModel(
             DevicesMainView devicesMainView,
             UsersMainView usersMainView
@@ -17,13 +12,6 @@ namespace InvMan.Desktop.UI.ViewModels
         {
             DevicesMainView = devicesMainView;
             UsersMainView = usersMainView;
-            CurrentContent = DevicesMainView;
-        }
-
-        public UserControl CurrentContent
-        {
-            get => _currentContent;
-            set { this.RaiseAndSetIfChanged(ref _currentContent, value); }
         }
 
         public UserControl DevicesMainView { get; }

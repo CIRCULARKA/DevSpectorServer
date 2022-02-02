a7140a1337ebc0b6bb660a0568fb123c86c8edce
CIRCULARKA <donkardon@list.ru>
2022-02-02 19:14:26 +0500

    Move views to proper place

---
+++ Desktop/InvMan.Desktop.UI/Views/UsersSection/UsersListView.axaml.cs.cs
@@ -0,0 +1,12 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using InvMan.Desktop.UI.ViewModels;
+
+namespace InvMan.Desktop.UI.Views
+{
+    public partial class UsersListView : UserControl
+    {
+        public UsersListView() { }
+    }
+}

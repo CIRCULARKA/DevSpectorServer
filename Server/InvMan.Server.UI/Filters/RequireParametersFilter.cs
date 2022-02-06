01e154a134596daff0452e8a132a2bf5167f429d
CIRCULARKA <donkardon@list.ru>
2022-02-06 22:20:11 +0500

    Create action filter

---
+++ Server/InvMan.Server.UI/Filters/RequireParametersFilter.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace InvMan.Server.UI.Filters
+{
+    public class RequireParametersAttribute : ActionFilterAttribute
+    {
+        private readonly string[] _requiredParameters;
+
+        public RequireParametersAttribute(params string[] parameters)
+        {
+            _requiredParameters = parameters;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            var missingParameters = new List<string>();
+
+            foreach (var param in _requiredParameters)
+                if (!context.HttpContext.Request.Query.ContainsKey(param))
+                    missingParameters.Add(param);
+
+            if (missingParameters.Count == 0)
+                return;
+
+            context.Result = new BadRequestObjectResult(
+                new {
+                    Error = "Some required parameters are missing",
+                    RequiredParameters = missingParameters
+                }
+            );
+        }
+    }
+}

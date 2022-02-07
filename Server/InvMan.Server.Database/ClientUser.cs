6143b3b63458fe0e149f05b738039c76eecdc39d
CIRCULARKA <donkardon@list.ru>
2022-02-07 13:42:10 +0500

    Return Group prop

--- Server/InvMan.Server.Database/ClientUser.cs
+++ Server/InvMan.Server.Database/ClientUser.cs
@@ -5,5 +5,7 @@ namespace InvMan.Server.Domain.Models
     public class ClientUser : IdentityUser
     {
         public string AccessKey { get; init; }
+
+        public string Group { get; init; }
     }
 }

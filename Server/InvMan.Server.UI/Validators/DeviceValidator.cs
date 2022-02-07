5569b089f74b395fd7f1f18307b92c52ad29b428
CIRCULARKA <donkardon@list.ru>
2022-02-07 14:02:47 +0500

    Change validator conditions

--- Server/InvMan.Server.UI/Validators/DeviceValidator.cs
+++ Server/InvMan.Server.UI/Validators/DeviceValidator.cs
@@ -11,8 +11,8 @@ namespace InvMan.Server.UI.Validators
             RuleFor(d => d.LocationID).NotEmpty();
 
             RuleFor(d => d.InventoryNumber).
-                NotEmpty().Length(10, 20);
-            RuleFor(d => d.NetworkName).NotEmpty().Length(3, 40);
+                Length(3, 20);
+            RuleFor(d => d.NetworkName).Length(3, 40);
         }
     }
 }

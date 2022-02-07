445a6ee40e0eeb820259937e68d0f25fea8d3d17
CIRCULARKA <donkardon@list.ru>
2022-02-07 13:41:11 +0500

    Remove Group property and rename file

--- Server/InvMan.Server.Database/ClientUser.cs
+++ Server/InvMan.Server.Database/ClientUser.cs
@@ -5,7 +5,5 @@ namespace InvMan.Server.Domain.Models
     public class ClientUser : IdentityUser
     {
         public string AccessKey { get; init; }
-
-        public string Group { get; init; }
     }
 }

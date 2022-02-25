713a41906d7fe8828ebd7bfa86fd4799cd2776d2
CIRCULARKA <donkardon@list.ru>
2022-02-25 11:32:54 +0500

    Refactor class

--- src/DevSpector.Domain/Models/Device.cs
+++ src/DevSpector.Domain/Models/Device.cs
@@ -6,16 +6,19 @@ namespace DevSpector.Domain.Models
 	{
 		public Guid ID { get; set; }
 
-		public string InventoryNumber { get; set; }
-
 		public Guid TypeID { get; set; }
 
-		public DeviceType Type { get; set; }
-
-		public string NetworkName { get; set; }
+		public Guid CabinetID { get; set; }
 
 		public Guid LocationID { get; set; }
 
+		public DeviceType Type { get; set; }
+
 		public Location Location { get; set; }
+
+		public string InventoryNumber { get; set; }
+
+		public string NetworkName { get; set; }
+
 	}
 }

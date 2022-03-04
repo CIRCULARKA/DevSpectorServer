b4b90e8a743d8c4164d29ff0015d71b9d6bd2d7b
CIRCULARKA <donkardon@list.ru>
2022-03-04 10:00:20 +0500

    Update Manager according to interface

--- src/DevSpector.Application/DevicesManager.cs
+++ src/DevSpector.Application/DevicesManager.cs
@@ -16,11 +16,11 @@ namespace DevSpector.Application
 			_repo = repo;
 		}
 
-		public void CreateDevice(Device device)
+		public void CreateDevice(DeviceInfo info)
 		{
 			// InventoryNumber should be unique for each device
 			// So if there is the device with same InventoryNumber then throw the exception
-			var sameDevice = _repo.GetSingle<Device>(d => d.InventoryNumber == device.InventoryNumber);
+			var sameDevice = _repo.GetSingle<Device>(d => d.InventoryNumber == info.InventoryNumber);
 			if (sameDevice != null)
 				throw new ArgumentException("Device with the same inventory number already exists");
 
@@ -31,15 +31,15 @@ namespace DevSpector.Application
 
 			// Try to get device type from specified ID
 			// If no type then throw the exception
-			var targetType = _repo.GetSingle<DeviceType>(dt => dt.ID == device.TypeID);
+			var targetType = _repo.GetSingle<DeviceType>(dt => dt.ID == info.TypeID);
 			if (targetType == null)
 				throw new ArgumentException("Device type with specified ID wasn't found");
 
 			var newDevice = new Device()
 			{
-				InventoryNumber = device.InventoryNumber,
+				InventoryNumber = info.InventoryNumber,
 				TypeID = targetType.ID,
-				NetworkName = device.NetworkName
+				NetworkName = info.NetworkName
 			};
 
 			_repo.Add<Device>(newDevice);
@@ -55,58 +55,58 @@ namespace DevSpector.Application
 			_repo.Save();
 		}
 
-		public void UpdateDevice(Device device)
+		public void UpdateDevice(DeviceInfo info)
 		{
 			// Check for device's persistance in database
 			// If there is no such device then trow the exception
-			var targetDevice = _repo.GetSingle<Device>(d => d.ID == device.ID);
+			var targetDevice = _repo.GetSingle<Device>(d => d.InventoryNumber == info.InventoryNumber);
 			if (targetDevice == null)
 				throw new ArgumentException("Could not update device with specified ID - no such device");
 
 			// Check if specified type is existing if device type ID is specified
 			DeviceType targetType;
-			if (device.TypeID != Guid.Empty) {
-				targetType = _repo.GetByID<DeviceType>(device.TypeID);
+			if (info.TypeID != Guid.Empty) {
+				targetType = _repo.GetByID<DeviceType>(info.TypeID);
 				if (targetType == null)
 					throw new ArgumentException("Can't assign device type to device - no such type with the specified ID");
-				targetDevice.TypeID = device.TypeID;
+				targetDevice.TypeID = info.TypeID;
 			}
 
-			if (device.InventoryNumber != null) {
+			if (info.InventoryNumber != null) {
 				// Check if there is already device with such inventory number
-				var sameDevice = _repo.GetSingle<Device>(d => d.InventoryNumber == device.InventoryNumber);
+				var sameDevice = _repo.GetSingle<Device>(d => d.InventoryNumber == info.InventoryNumber);
 				if (sameDevice != null)
 					throw new ArgumentException("Can't update device - there is already device with inventory number specified");
 
-				targetDevice.InventoryNumber = device.InventoryNumber;
+				targetDevice.InventoryNumber = info.InventoryNumber;
 			}
 
-			if (device.NetworkName != null) {
+			if (info.NetworkName != null) {
 				// Check if there is already device with such network name
-				var sameDevice = _repo.GetSingle<Device>(d => d.NetworkName == device.NetworkName);
+				var sameDevice = _repo.GetSingle<Device>(d => d.NetworkName == info.NetworkName);
 				if (sameDevice != null)
 					throw new ArgumentException("Can't update device - there is already device with network name specified");
-				targetDevice.NetworkName = device.NetworkName;
+				targetDevice.NetworkName = info.NetworkName;
 			}
 
 			_repo.Update<Device>(targetDevice);
 			_repo.Save();
 		}
 
-		public Device GetDeviceByID(Guid deviceID) =>
-			_repo.GetByID<Device>(deviceID);
+		public Device GetDeviceByInventoryNumber(string invNum) =>
+			_repo.GetSingle<Device>(d => d.InventoryNumber == invNum);
 
 		public IEnumerable<Device> GetDevices() =>
 			_repo.Get<Device>(include: "Type");
 
-		public Cabinet GetDeviceCabinet(Guid deviceID) =>
-			_repo.GetSingle<DeviceCabinet>(include: "Cabinet,Cabinet.Housing",
-				filter: dc => dc.DeviceID == deviceID).Cabinet;
+		public Cabinet GetDeviceCabinet(string inventoryNumber) =>
+			_repo.GetSingle<DeviceCabinet>(include: "Cabinet,Cabinet.Housing,Device",
+				filter: dc => dc.Devcie.InventoryNumber == inventoryNumber).Cabinet;
 
 		public IEnumerable<Appliance> GetAppliances()
 		{
 			return GetDevices().Select(d => {
-				var deviceCabinet = GetDeviceCabinet(d.ID);
+				var deviceCabinet = GetDeviceCabinet(d.InventoryNumber);
 				return new Appliance(
 					d.ID,
 					d.InventoryNumber,

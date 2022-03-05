4558605befee41ed7bd0bae822c5a79b4b57a7bc
CIRCULARKA <donkardon@list.ru>
2022-03-05 10:32:10 +0500

    Add new DTO

---
+++ src/DevSpector.Application/SoftwareInfo.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DevSpector.Application
+{
+    /// <summary>
+    /// This is the DTO object needed to provide information from HTTP request to controller's action methods
+    /// Defines contract between clients and server
+    /// </summary>
+    public class SoftwareInfo
+    {
+        public string DeviceInventoryNumber { get; set; }
+
+        public string SoftwareName { get; set; }
+
+        public string SoftwareVersion { get; set; }
+    }
+}

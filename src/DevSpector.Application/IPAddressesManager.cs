51999090347af69fea87682a3eb60adebf9ef436
CIRCULARKA <donkardon@list.ru>
2022-03-06 10:27:00 +0500

    Move logic that defines whether the IP is match the pattern and is IP is free to IPManager

--- src/DevSpector.Application/IPAddressesManager.cs
+++ src/DevSpector.Application/IPAddressesManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using DevSpector.Domain;
 using DevSpector.Domain.Models;
@@ -25,5 +27,18 @@ namespace DevSpector.Application
 					ThenBy(address => int.Parse(address.Split(".")[1])).
 					ThenBy(address => int.Parse(address.Split(".")[2])).
 					ThenBy(address => int.Parse(address.Split(".")[3]));
+
+		public bool IsAddressFree(string ipAddress)
+		{
+			if (!MathesIPv4(ipAddress))
+				throw new ArgumentException("IP address does not match IPv4 pattern");
+
+			return GetFreeIP().Contains(ipAddress);
+		}
+
+		public bool MathesIPv4(string ipAddress) =>
+			new Regex("\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.|$)){4}\b").
+				IsMatch(ipAddress);
+
 	}
 }

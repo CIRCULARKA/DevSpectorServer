135b99230a5b19b0243d3d27e14f0a2833856b1c
CIRCULARKA <donkardon@list.ru>
2022-03-13 12:28:28 +0500

    Add new interface for generating range of IP addresses

---
+++ src/DevSpector.Application/Networking/IIPRangeGenerator.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using DevSpector.Domain.Models;
+
+namespace DevSpector.Application.Networking
+{
+	/// <summary>
+	/// Class for generating the range of IP addresses using specified mask
+	/// </summary>
+	public interface IIPRangeGenerator
+	{
+		IList<IPAddress> GenerateRange(string netwokAddress, int mask);
+	}
+}

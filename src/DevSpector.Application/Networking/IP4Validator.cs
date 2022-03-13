742d9e46caa602f6323eb877d13a01b32ad4d913
CIRCULARKA <donkardon@list.ru>
2022-03-13 12:57:45 +0500

    Change implementation according to interface

--- src/DevSpector.Application/Networking/IP4Validator.cs
+++ src/DevSpector.Application/Networking/IP4Validator.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Text.RegularExpressions;
+using DevSpector.Application.Networking.Enumerations;
 
 namespace DevSpector.Application.Networking
 {
-    public class IP4Validator : IIPValidator
+    public class IPValidator : IIPValidator
     {
         private const string _Ip4Pattern =
             @"^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$";
 
-		public bool Matches(string value)
+		public bool Matches(string value, IPProtocol protocolVersion)
         {
+            if (protocolVersion != IPProtocol.Version4)
+                throw new NotImplementedException("Can't validate IP address - only IPv4 supported");
+
 			try { return Regex.IsMatch(value, _Ip4Pattern); }
             catch (ArgumentNullException)
             {

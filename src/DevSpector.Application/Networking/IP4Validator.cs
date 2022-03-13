dc12d197130ede13eedefb26e687408630b3c1d4
CIRCULARKA <donkardon@list.ru>
2022-03-13 12:42:47 +0500

    Make implementation inherit from interface

--- src/DevSpector.Application/Networking/IP4Validator.cs
+++ src/DevSpector.Application/Networking/IP4Validator.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 
 namespace DevSpector.Application.Networking
 {
-    public class IP4Validator
+    public class IP4Validator : IIPValidator
     {
         private const string _Ip4Pattern =
             @"^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$";

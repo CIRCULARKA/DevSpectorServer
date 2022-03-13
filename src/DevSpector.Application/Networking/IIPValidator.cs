065dd6effb28a91758c8e1adba01a3d59798650b
CIRCULARKA <donkardon@list.ru>
2022-03-13 12:55:43 +0500

    Update interface of IP validation

--- src/DevSpector.Application/Networking/IIPValidator.cs
+++ src/DevSpector.Application/Networking/IIPValidator.cs
@@ -1,7 +1,9 @@
+using DevSpector.Application.Networking.Enumerations;
+
 namespace DevSpector.Application.Networking
 {
     public interface IIPValidator
     {
-        bool Matches(string value);
+        bool Matches(string value, IPProtocol protocolVersion);
     }
 }

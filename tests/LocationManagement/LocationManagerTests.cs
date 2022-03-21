3d0b3d577e27080563732d72a666c296eb1050f6
CIRCULARKA <donkardon@list.ru>
2022-03-21 14:03:15 +0500

    Add test for GetHousingCabinets method

--- tests/LocationManagement/LocationManagerTests.cs
+++ tests/LocationManagement/LocationManagerTests.cs
@@ -34,5 +34,42 @@ namespace DevSpector.Tests.Application.Location
                 Assert.Equal(expected[i].Name, actual[i].Name);
             }
         }
+
+        [Fact]
+        public void ReturnsHousingCabinets()
+        {
+            // Arrange
+            var housings = _context.Housings.ToList();
+
+            var expected = new List<List<Cabinet>>();
+            for (int i = 0; i < housings.Count; i++)
+            {
+                List<Cabinet> cabinets = _context.Cabinets.Where(c => c.HousingID == housings[i].ID).ToList();
+                expected.Add(cabinets);
+            }
+
+            // Act
+            var actual = new List<List<Cabinet>>();
+
+            for (int i = 0; i < housings.Count; i++)
+            {
+                List<Cabinet> actualCabinets = _manager.GetCabinets(housings[i].ID);
+                actual.Add(actualCabinets);
+            }
+
+            // Assert
+            Assert.Equal(expected.Count, actual.Count);
+            for (int i = 0; i < housings.Count; i++)
+            {
+                Assert.Equal(expected[i].Count, actual[i].Count);
+                for (int j = 0; j < expected[i].Count; j++)
+                {
+                    Assert.Equal(expected[i][j].ID, actual[i][j].ID);
+                    Assert.Equal(expected[i][j].Name, actual[i][j].Name);
+                    Assert.Equal(expected[i][j].HousingID, actual[i][j].HousingID);
+                    Assert.NotNull(actual[i][j].Housing);
+                }
+            }
+        }
     }
 }

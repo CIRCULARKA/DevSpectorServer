a7086278a9f987eec426d726a1072201c0b87cee
CIRCULARKA <donkardon@list.ru>
2022-03-21 13:43:09 +0500

    Trim unused namespace

--- tests/DevicesManagement/DeviceEditorTests.cs
+++ tests/DevicesManagement/DeviceEditorTests.cs
@@ -1,15 +1,11 @@
 using System;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 using Xunit;
 using DevSpector.Tests.Database;
 using DevSpector.Application.Devices;
-using DevSpector.Domain;
 using DevSpector.Domain.Models;
 using DevSpector.Database;
 using DevSpector.Application.Networking;
-using DevSpector.SDK.Models;
 
 namespace DevSpector.Tests.Application.Devices
 {

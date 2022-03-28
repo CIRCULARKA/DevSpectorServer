1ff60f38a6114e30ffb77603e6e95cbf85e2a925
CIRCULARKA <donkardon@list.ru>
2022-03-28 10:31:27 +0500

    Create new inermediate table for devices and its IPs

---
+++ src/DevSpector.Domain/Models/DeviceIPAddress.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DevSpector.Domain.Models
+{
+	public class DeviceIPAddress
+	{
+		public Guid ID { get; set; }
+
+		public Guid IPAddressID { get; set; }
+
+        public Guid DeviceID { get; set; }
+
+        public IPAddress IPAddress { get; set; }
+
+        public Device Device { get; set; }
+	}
+}

fb850b0081e6b1f08f8bfd8484df11e8a87ec27d
CIRCULARKA <donkardon@list.ru>
2022-04-19 18:38:51 +0500

    Add validation attributes for all other "input" DTOs

--- src/DevSpector.Database/DTO/Input/LANInfo.cs
+++ src/DevSpector.Database/DTO/Input/LANInfo.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 
 namespace DevSpector.Database.DTO
 {
     public class LANInfo
     {
+        [Required(ErrorMessage = "сетевой адрес не был передан", AllowEmptyStrings = false)]
         public string NetworkAddress { get; set; }
 
+        [Required(ErrorMessage = "маска подсети не была передана")]
         public int Mask { get; set; }
     }
 }
--- src/DevSpector.Database/DTO/Input/UserToAdd.cs
+++ src/DevSpector.Database/DTO/Input/UserToAdd.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DevSpector.Database.DTO
 {
@@ -8,14 +9,21 @@ namespace DevSpector.Database.DTO
     /// </summary>
     public class UserToAdd
     {
+        [StringLength(100, ErrorMessage = "длина имени должна находиться между {2} и {1} символами", MinimumLength = 2)]
         public string FirstName { get; init; }
 
+        [StringLength(100, ErrorMessage = "длина фамилии должна находиться между {2} и {1} символами", MinimumLength = 2)]
         public string Surname { get; init; }
 
+        [StringLength(100, ErrorMessage = "длина отчества должна находиться между {2} и {1} символами", MinimumLength = 2)]
         public string Patronymic { get; init; }
 
+        [Required(ErrorMessage = "логин не был предоставлен", AllowEmptyStrings = false)]
+        [StringLength(100, ErrorMessage = "длина логина должна находиться между {2} и {1} символами", MinimumLength = 2)]
         public string Login { get; init; }
 
+        [Required(ErrorMessage = "пароль не был предоставлен", AllowEmptyStrings = false)]
+        [StringLength(100, ErrorMessage = "длина пароля должна находиться между {2} и {1} символами", MinimumLength = 5)]
         public string Password { get; init; }
 
         public Guid GroupID { get; init; }

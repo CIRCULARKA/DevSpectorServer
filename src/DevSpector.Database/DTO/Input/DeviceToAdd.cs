0297aadbe62e5cbdb49e23c3049ae00876403658
CIRCULARKA <donkardon@list.ru>
2022-04-19 18:45:29 +0500

    Write first letter of error in lower case (convenient for clients)

--- src/DevSpector.Database/DTO/Input/DeviceToAdd.cs
+++ src/DevSpector.Database/DTO/Input/DeviceToAdd.cs
@@ -9,17 +9,17 @@ namespace DevSpector.Database.DTO
     /// </summary>
     public class DeviceToAdd
     {
-        [StringLength(70, ErrorMessage = "Длина инвентарного номера должна быть между {2} и {1} символами", MinimumLength = 3)]
-        [Required(ErrorMessage = "Инвентарный номер должен быть указан", AllowEmptyStrings = false)]
+        [StringLength(70, ErrorMessage = "длина инвентарного номера должна быть между {2} и {1} символами", MinimumLength = 3)]
+        [Required(ErrorMessage = "инвентарный номер должен быть указан", AllowEmptyStrings = false)]
         public string InventoryNumber { get; set; }
 
-        [Required(ErrorMessage = "Идентификатор типа не был передан", AllowEmptyStrings = false)]
+        [Required(ErrorMessage = "идентификатор типа не был передан", AllowEmptyStrings = false)]
         public Guid TypeID { get; set; }
 
-        [StringLength(50, ErrorMessage = "Длина сетевого имени должна быть между {2} и {1} символами", MinimumLength = 3)]
+        [StringLength(50, ErrorMessage = "длина сетевого имени должна быть между {2} и {1} символами", MinimumLength = 3)]
         public string NetworkName { get; set; }
 
-        [StringLength(100, ErrorMessage = "Длина названия должна быть между {2} и {1} символами", MinimumLength = 1)]
+        [StringLength(100, ErrorMessage = "длина названия должна быть между {2} и {1} символами", MinimumLength = 1)]
         public string ModelName { get; set; }
     }
 }
